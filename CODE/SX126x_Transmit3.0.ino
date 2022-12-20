70f0a26eb044b00f5b81f809b3aa437cc5130542
BenzaouiaOussama <71930590+OussamaBenzaoui@users.noreply.github.com>
2022-12-20 08:43:19 +0100

    Add files via upload

---
+++ CODE/SX126x_Transmit3.0.ino
@@ -0,0 +1,120 @@
+/*
+   RadioLib SX126x Transmit Example
+
+   This example transmits packets using SX1262 LoRa radio module.
+   Each packet contains up to 256 bytes of data, in the form of:
+    - Arduino String
+    - null-terminated char array (C-string)
+    - arbitrary binary data (byte array)
+
+   Other modules from SX126x family can also be used.
+
+   For default module settings, see the wiki page
+   https://github.com/jgromes/RadioLib/wiki/Default-configuration#sx126x---lora-modem
+
+   For full API reference, see the GitHub Pages
+     https://jgromes.github.io/RadioLib/
+
+SX126x - LoRa modem
+Begin method:
+Carrier frequency: 434.0 MHz
+Bandwidth: 125.0 kHz (dual-sideband)
+Spreading factor: 9
+Coding rate: 4/7
+Sync word: SX126X_SYNC_WORD_PRIVATE (0x12)
+Output power: 10 dBm
+Preamble length: 8 symbols
+
+
+
+*/
+
+// include the library
+#include <RadioLib.h>
+
+// SX1262 has the following connections:
+// NSS pin :  5
+// DIO1 pin:  12
+// DIO0 pin:  26
+// NRST pin:  14
+// BUSY pin:  4
+SX1262 radio = new Module(5,12,14,4);
+
+// or using RadioShield
+// https://github.com/jgromes/RadioShield
+//SX1262 radio = RadioShield.ModuleA;
+
+// or using CubeCell
+//SX1262 radio = new Module(RADIOLIB_BUILTIN_MODULE);
+
+void setup() {
+  Serial.begin(115200);
+
+  // initialize SX1262 with default settings
+  Serial.print(F("[SX1262] Initializing ... "));
+  int state = radio.begin(868.0, 125.0, 9, 7, 0x12,10,8,1.6,false);
+  radio.setDio2AsRfSwitch(true);
+  radio.explicitHeader();
+  radio.setCRC(2);
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true);
+  }
+
+  // some modules have an external RF switch
+  // controlled via two pins (RX enable, TX enable)
+  // to enable automatic control of the switch,
+  // call the following method
+  // RX enable:   4
+  // TX enable:   5
+    
+    radio.setRfSwitchPins(4, 5);
+  
+}
+
+void loop() {
+  Serial.print(F("[SX1262] Transmitting packet ... "));
+
+  // you can transmit C-string or Arduino string up to
+  // 256 characters long
+  // NOTE: transmit() is a blocking method!
+  //       See example SX126x_Transmit_Interrupt for details
+  //       on non-blocking transmission method.
+  int state = radio.transmit("Hello World!");
+
+  // you can also transmit byte array up to 256 bytes long
+  /*
+    byte byteArr[] = {0x01, 0x23, 0x45, 0x56, 0x78, 0xAB, 0xCD, 0xEF};
+    int state = radio.transmit(byteArr, 8);
+  */
+
+  if (state == RADIOLIB_ERR_NONE) {
+    // the packet was successfully transmitted
+    Serial.println(F("success!"));
+
+    // print measured data rate
+    Serial.print(F("[SX1262] Datarate:\t"));
+    Serial.print(radio.getDataRate());
+    Serial.println(F(" bps"));
+
+  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
+    // the supplied packet was longer than 256 bytes
+    Serial.println(F("too long!"));
+
+  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
+    // timeout occured while transmitting packet
+    Serial.println(F("timeout!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+
+  }
+
+  // wait for a second before transmitting again
+  delay(1000);
+}
